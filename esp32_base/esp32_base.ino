8900074fc789fff657b2798918010f9946621b04
Kaloyan-Pashov <pashov.kaloyan@gmail.com>
2021-04-11 01:38:19 +0300

    added temperature sensor functionality

--- esp32_base/esp32_base.ino
+++ esp32_base/esp32_base.ino
@@ -2,6 +2,16 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+//temp_sens
+#define THERMISTORNOMINAL 10000
+#define TEMPERATURENOMINAL 21
+#define BCOEFFICIENT 3950
+#define SERIESRESISTOR 10000
+
+//pins
+int ledPin = 32;
+
+float average;
 
 //wifi
 char* ssid = "7702knuprebyC";
@@ -9,14 +19,17 @@ char* password = "3>ciM6Q#~_";
 IPAddress ip;
 
 void setup() {
+  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
   wifi();
-  delay(1000);
-
+  delay(1000);  
 }
 
 void loop() {
   json();
+  int temp_s  = measure_temp();
+  if (temp_s < 24) low_temp();
+  else digitalWrite(ledPin, LOW);
 }
 
 
@@ -42,5 +55,27 @@ void json() {
   String payload = http.getString();
   Serial.println(payload);
   http.end();
-  delay(30000);
+  delay(1000);
+}
+
+int measure_temp() {
+    average = analogRead(33);
+    average = 4095 / average - 1;
+    average = SERIESRESISTOR / average;
+    float temperature;
+    temperature = average / THERMISTORNOMINAL; 
+    temperature = log(temperature);
+    temperature /= BCOEFFICIENT;
+    temperature += 1.0 / (TEMPERATURENOMINAL + 273.15); 
+    temperature = 1.0 / temperature;
+    temperature -= 273.15; 
+    Serial.print("Temperature: "); 
+    Serial.print(temperature);
+    Serial.println("°C");   
+    return (temperature);
+}
+
+void low_temp() { 
+  ///when the temp is low, turn on the heater and the signalling led, send notification
+  digitalWrite(ledPin, HIGH);
 }
